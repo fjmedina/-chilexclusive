1b853f4e707166b9ec927dd368cfc13536fb0443
Francisco Medina <fco.medinag@gmail.com>
2024-11-18 23:34:44 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Hero } from './components/Hero';
 import { ContactForm } from './components/ContactForm';
 import { Footer } from './components/Footer';
 import { AdminPanel } from './components/admin/AdminPanel';
+import { AdminLogin } from './components/admin/AdminLogin';
 import { AuthProvider, useAuth } from './context/AuthContext';
 
 function AppContent() {
